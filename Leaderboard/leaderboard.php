0a6ed8f3da11721d42ff803926239e997ccda1ee
LHTY03 <143739953+LHTY-RPI@users.noreply.github.com>
2024-11-08 12:51:49 -0500

    add include files for header.php

--- Leaderboard/leaderboard.php
+++ Leaderboard/leaderboard.php
@@ -8,13 +8,10 @@ session_start();
         <meta charset="UTF-8">
         <title>LeaderBoard</title>
         <link rel="stylesheet" href="../assets/MainPage.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="./src/leaderboard.css">
-        <link rel="stylesheet" href="../assets/navbar.css">
         <script src="../assets/MainPage.js" defer></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="./src/leaderboard.js" defer></script>
-
+        <?php include "../php/include_header.php"?>
     </head>
     <body>
         
