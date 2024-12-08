6928eb81b248e1e19c324790ea4a2ba5a577f72c
LHTY03 <143739953+LHTY-RPI@users.noreply.github.com>
2024-12-07 23:03:10 -0500

    change prediction

--- Predictions/Predictions.php
+++ Predictions/Predictions.php
@@ -8,7 +8,6 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Predictions</title>
-    <link rel="stylesheet" href="../assets/MainPage.css">
     <link rel="stylesheet" href="./src/predictions.css">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     <?php include "../php/include_header.php"?>
