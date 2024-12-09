7578d600df0e4e3d81d9b3d389c4d84cad31b7c4
sharmp5 <sharmp5@rpi.edu>
2024-12-09 14:44:21 -0500

    CSS without 2 files
    Left out leaderboard.css and predictions.css as they caused conflicts

--- Predictions/Predictions.php
+++ Predictions/Predictions.php
@@ -8,7 +8,6 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Predictions</title>
-    <link rel="stylesheet" href="../assets/MainPage.css">
     <link rel="stylesheet" href="./src/predictions.css">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     <?php include "../php/include_header.php"?>
@@ -20,45 +19,14 @@ session_start();
 
 <header id="header1">Predictions</header>
 <div id="mainframe">
-    <div>
-        <img src="../assets/Photos/teamLogo/hawks.png" alt="Left Image 1">
-        <div class="content">
-            <h2>Wednesday, 10/21</h2>
-            <p>Semi-Finals</p>
-        </div>
-        <img src="../assets/Photos/teamLogo/hornets.png" alt="Left Image 1">
-    </div>
 
-    <div>
-        <img src="../assets/Photos/teamLogo/nets.png" alt="Left Image 1">
-        <div class="content">
-            <h2>Thursday, 10/22</h2>
-            <p>Semi-Finals</p>
-        </div>
-        <img src="../assets/Photos/teamLogo/magic.png" alt="Left Image 1">
-    </div>
+    <div id="Recent_Results">
 
-    <div>
-        <img src="../assets/Photos/teamLogo/nets.png" alt="Left Image 1">
-        <div class="content">
-            <h2>Friday, 10/23</h2>
-            <p>Finals</p>
-        </div>
-        <img src="../assets/Photos/teamLogo/hawks.png" alt="Left Image 1">
-    </div>
-
-    <div>
-        <img src="../assets/Photos/teamLogo/nets.png" alt="Left Image 1">
-        <div class="content">
-            <h2>Friday, 10/23</h2>
-            <p>Finals</p>
-        </div>
-        <img src="../assets/Photos/teamLogo/hawks.png" alt="Left Image 1">
     </div>
 
 </div>
 
 
-
+<script src="./src/predictions.js"></script>
 </body>
 </html>
